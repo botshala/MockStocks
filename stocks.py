981dd21bf9ddc3cd5233d06627ed606289b30e2e
rajat2797 <rajatsinghal1997@gmail.com>
2016-09-29 23:17:19 +0530

    updates

--- stocks.py
+++ stocks.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 def get_code(name):
 	get_code = 'http://dev.markitondemand.com/MODApis/Api/v2/Lookup?input=%s'%(name)
-	soup=bs(requests.get(get_code).text,"lxml")
+	soup=bs(requests.get(get_code).text)
 	# print soup
 	data = soup.find_all('symbol')
 	# print data
