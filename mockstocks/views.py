d7ef2981215643b91aaa02c8ecc9c9fff0913730
rajat2797 <rajatsinghal1997@gmail.com>
2016-09-30 19:20:50 +0530

    updates

--- mockstocks/views.py
+++ mockstocks/views.py
@@ -16,15 +16,51 @@ def stock(message):
 	url = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=%s&callback=myFunction'%(code)
 	resp = requests.get(url=url).text.split('(')[1].split(')')[0]
 	data=json.loads(resp)
-	image_url = 'http://stockcharts.com/c-sc/sc?s=%s&p=D&b=5&g=0&i=t15810600769&r=1475241538081'%(code)
 	output_text = ''
 	output_text = 'Name: %s\nSymbol: %s\nOpen: %s\nLast Price: %s\nChange Percent: %s\nHigh: %s\nLow: %s'%(data['Name'],data['Symbol'],data['Open'],data['LastPrice'],data['ChangePercent'],data['High'],data['Low'])
-	return image_url, output_text
+	return output_text
 
 def post_fb_msg(fbid,message):
 	post_fb_url='https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
-	image_url, output_text = stock(message)
+	output_text = stock(message)
+	code = stocks.get_code(message)
 	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text": output_text }})
+	response_msg_quick = {
+				"recipient":{
+				    "id":fbid
+				  },
+				  "message":{
+				    "text":"View Chart",
+				    "quick_replies":[
+				      {
+				        "content_type":"text",
+				        "title":"Daily",
+				        "payload":"daily"
+				      },
+				      {
+				        "content_type":"text",
+				        "title":"Weekly",
+				        "payload":"weekly"
+				      }
+				    ]
+				  }
+	}
+	
+	response_msg_quick = json.dumps(response_msg_quick)
+	status1 = requests.post(post_fb_url, headers={"Content-Type": "application/json"},data=response_msg)
+	status2 = requests.post(post_fb_url, headers={"Content-Type": "application/json"},data=response_msg_quick)
+	print status1.json()
+	print status2.json()
+
+	print "****payload=%s"%payload
+
+	if not payload:
+		return
+	elif payload == "daily":
+		image_url = 'http://stockcharts.com/c-sc/sc?s=%s&p=D&b=5&g=0&i=t15810600769&r=1475241538081'%(code)
+	else:
+		image_url = 'http://stockcharts.com/c-sc/sc?s=%s&p=W&b=5&g=0&i=t57136307326&r=1475242265838'%(code)
+
 	response_msg_image = {
 				"recipient":{
 				    "id":fbid
@@ -39,10 +75,8 @@ def post_fb_msg(fbid,message):
 				  }
 	}
 	response_msg_image = json.dumps(response_msg_image)
-	status1 = requests.post(post_fb_url, headers={"Content-Type": "application/json"},data=response_msg)
-	status2 = requests.post(post_fb_url, headers={"Content-Type": "application/json"},data=response_msg_image)
-	print status1.json()
-	print status2.json()
+	status3 = requests.post(post_fb_url, headers={"Content-Type": "application/json"},data=response_msg_image)
+	print status3.json()
 
 class MyChatBotView(generic.View):
 	def get(self,request,*args,**kwargs):
